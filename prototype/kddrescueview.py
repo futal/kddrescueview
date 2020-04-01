e28e70a71ea341c96d3a9e332b28fd54d3166d81
Adrien Cordonnier <adrien.cordonnier@gmail.com>
2020-04-01 11:33:34 +0300

    Access texture with integer coordinates
    Texture lookup is performed using integer texel coordinates instead
    of normalized coordinates on the range [0, 1). Texture size is
    passed as a uniform to the shader.

--- prototype/kddrescueview.py
+++ prototype/kddrescueview.py
@@ -47,25 +47,31 @@ class Scene:
             fragment_shader='''
                 #version 330
 
+                uniform int tex_size;
                 uniform usampler3D tex;
 
                 in vec2 v_text;
                 out uvec4 gl_FragColor;
 
                 void main() {
-                    vec3 coord = vec3(v_text/2. + .5, 0.);
-                    uvec3 rgb = texture(tex, coord).rgb;
+                    ivec3 coord = ivec3((v_text+1.)*tex_size/2., 0.);
+                    uvec3 rgb = texelFetch(tex, coord, 0).rgb;
                     gl_FragColor = uvec4(rgb, 255);
                 }
             ''',
         )
 
+
         self.vbo = ctx.buffer(reserve='4MB', dynamic=True)
         self.vao = ctx.simple_vertex_array(self.prog, self.vbo, 'vertices')
-        tex_data = np.random.randint(256, size=(256, 256, 256, 4), dtype=np.uint8)
-        self.tex = ctx.texture3d(size=(256, 256, 256), components=4, data=tex_data)
+        tex_size = 256
+        self.tex_size = self.prog['tex_size']
+        self.tex_size.value = tex_size
+        tex_data = np.random.randint(256, size=(tex_size,)*3+(4,), dtype=np.uint8)
+        self.tex = ctx.texture3d(size=(tex_size,)*3, components=4, data=tex_data)
         self.tex.filter = (moderngl.NEAREST, moderngl.NEAREST)
         self.tex.use()
+
 
 
     def clear(self, color=(0, 0, 0, 0)):
